ef354578e3c22386091142b20e1b60027761ca0c
Cihat Gündüz <commits@cihatguenduez.de>
2020-04-10 23:56:40 +0200

    Update Formula to version 0.2.0

--- Formula/anylint.rb
+++ Formula/anylint.rb
@@ -1,10 +1,10 @@
 class Anylint < Formula
   desc "Lint anything by combining the power of Swift & regular expressions"
   homepage "https://github.com/Flinesoft/AnyLint"
-  url "https://github.com/Flinesoft/AnyLint.git", :tag => "0.1.1", :revision => "e80ac907d160a0e8f359dc84fabfbd1cc80a8b50"
+  url "https://github.com/Flinesoft/AnyLint.git", :tag => "0.2.0", :revision => "c5fd1ad0987da7fb6c895ba5ca90acf51a5f0b14"
   head "https://github.com/Flinesoft/AnyLint.git"
 
-  depends_on :xcode => ["11.3", :build]
+  depends_on :xcode => ["11.4", :build]
 
   def install
     system "make", "install", "prefix=#{prefix}"
