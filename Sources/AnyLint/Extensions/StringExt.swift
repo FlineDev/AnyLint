2aa85fc354ec9f5705a6200ec17b6187d88acdfd
Cihat Gündüz <commits@cihatguenduez.de>
2020-06-08 23:49:58 +0200

    Make extension APIs public

--- Sources/AnyLint/Extensions/StringExt.swift
+++ Sources/AnyLint/Extensions/StringExt.swift
@@ -8,7 +8,7 @@ extension String {
     /// Info about the exact location of a character in a given file.
     public typealias LocationInfo = (line: Int, charInLine: Int)
 
-    func locationInfo(of index: String.Index) -> LocationInfo {
+    public func locationInfo(of index: String.Index) -> LocationInfo {
         let prefix = self[startIndex ..< index]
         let prefixLines = prefix.components(separatedBy: .newlines)
         guard let lastPrefixLine = prefixLines.last else { return (line: 1, charInLine: 1) }
--- Sources/AnyLint/Extensions/URLExt.swift
+++ Sources/AnyLint/Extensions/URLExt.swift
@@ -2,7 +2,8 @@ import Foundation
 import Utility
 
 extension URL {
-    var relativePathFromCurrent: String {
+    /// Returns the relative path of from the current path.
+    public var relativePathFromCurrent: String {
         String(path.replacingOccurrences(of: fileManager.currentDirectoryPath, with: "").dropFirst())
     }
 }
