b1c3ba9f6ae27bee3d8726a24a9b2ec9f6192cc5
Cihat Gündüz <commits@cihatguenduez.de>
2020-06-08 18:43:02 +0200

    Make allFiles method in `FilesSearch` publicly accessible

--- Sources/AnyLint/FilesSearch.swift
+++ Sources/AnyLint/FilesSearch.swift
@@ -9,7 +9,7 @@ public final class FilesSearch {
         let excludeFilters: [Regex]
     }
 
-    static let shared = FilesSearch()
+    public static let shared = FilesSearch()
 
     private var cachedFilePaths: [SearchOptions: [String]] = [:]
 
@@ -20,7 +20,8 @@ public final class FilesSearch {
         cachedFilePaths = [:]
     }
 
-    func allFiles(within path: String, includeFilters: [Regex], excludeFilters: [Regex] = []) -> [String] { // swiftlint:disable:this function_body_length
+    /// Returns all file paths within given `path` matching the given `include` and `exclude` filters.
+    public func allFiles(within path: String, includeFilters: [Regex], excludeFilters: [Regex] = []) -> [String] { // swiftlint:disable:this function_body_length
         log.message(
             "Start searching for matching files in path \(path) with includeFilters \(includeFilters) and excludeFilters \(excludeFilters) ...",
             level: .debug
