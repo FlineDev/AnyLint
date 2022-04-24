f14c86807106e9f1a2e62f76e2ef4bf40497156b
Cihat Gündüz <commits@cihatguenduez.de>
2022-04-24 23:40:05 +0200

    Point formula to correct tag commit hash

--- Formula/anylint.rb
+++ Formula/anylint.rb
@@ -1,7 +1,7 @@
 class Anylint < Formula
   desc "Lint anything by combining the power of Swift & regular expressions"
   homepage "https://github.com/FlineDev/AnyLint"
-  url "https://github.com/FlineDev/AnyLint.git", :tag => "0.9.0", :revision => "789e7320c9c70a90cc4f99fb245160d9ec62da7b"
+  url "https://github.com/FlineDev/AnyLint.git", :tag => "0.9.0", :revision => "77e24fdedb70413dd8f750799dedc4b309b7c8eb"
   head "https://github.com/FlineDev/AnyLint.git"
 
   depends_on :xcode => ["12.5", :build]
